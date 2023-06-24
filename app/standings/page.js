2cb405ffa83b3af2c73b27991491c01d733532a8
Tyler Martin <tyler.martin@electric-playground.com>
2023-06-23 23:58:25 -0300

    improves table layout for mobile devices

--- app/standings/page.js
+++ app/standings/page.js
@@ -16,8 +16,8 @@ const StandingsPage = async () => {
   }
 
   return (
-      <div className="p-24 overflow-x-auto">
-          <table className="min-w-full text-left text-sm font-light">
+      <div className="sm:p-24 overflow-x-auto">
+          <table className="min-w-full text-sm font-light">
           <caption><h2 className="border-solid border-2 border-indigo-600">Fantasy League</h2></caption>
           <thead className="border-b font-medium dark:border-neutral-500">
             <tr>
@@ -34,14 +34,14 @@ const StandingsPage = async () => {
           <tbody>
             {standings && standings.map(row => (
               <tr className="border-b dark:border-neutral-500" key={row.owner.name + row.predictions.percentage}>
-                <td className="whitespace-nowrap px-6 py-4">{row.owner.name}</td>
-                <td className="whitespace-nowrap px-6 py-4">{row.eventStats.totalNhlPoints + row.predictions.fantasyPoints}</td>
-                <td className="whitespace-nowrap px-6 py-4">{row.eventStats.totalNhlPoints}</td>
-                <td className="whitespace-nowrap px-6 py-4">{row.predictions.fantasyPoints}</td>
-                <td className="whitespace-nowrap px-6 py-4">{row.predictions.percentage}</td>
-                <td className="whitespace-nowrap px-6 py-4">{row.predictions.correct}</td>
-                <td className="whitespace-nowrap px-6 py-4">{row.predictions.incorrect}</td>
-                <td className="whitespace-nowrap px-6 py-4">{row.predictions.total}</td>
+                <td className="whitespace-nowrap px-3 py-2">{row.owner.name}</td>
+                <td className="whitespace-nowrap px-3 py-2">{row.eventStats.totalNhlPoints + row.predictions.fantasyPoints}</td>
+                <td className="whitespace-nowrap px-3 py-2">{row.eventStats.totalNhlPoints}</td>
+                <td className="whitespace-nowrap px-3 py-2">{row.predictions.fantasyPoints}</td>
+                <td className="whitespace-nowrap px-3 py-2">{row.predictions.percentage}</td>
+                <td className="whitespace-nowrap px-3 py-2">{row.predictions.correct}</td>
+                <td className="whitespace-nowrap px-3 py-2">{row.predictions.incorrect}</td>
+                <td className="whitespace-nowrap px-3 py-2">{row.predictions.total}</td>
               </tr>
             ))}
           </tbody>
