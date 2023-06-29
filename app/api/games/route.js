7812251089d1719858e474c28d20087cf1bfbf72
Tyler Martin <tyler.martin@electric-playground.com>
2023-06-28 21:46:16 -0300

    working on endpoint to return upcoming games, and creating form to create predictions

--- app/api/games/route.js
+++ app/api/games/route.js
@@ -2,13 +2,10 @@ import Owner from "@/lib/Owner";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
-  const searchParams = new URL(request.url)
+  const { searchParams } = new URL(request.url)
   const ownerId = parseInt(searchParams.get('ownerId'))
   const addDays = parseInt(searchParams.get('addDays'))
 
-  const res = Owner.upcomingGames(ownerId, addDays)
-
-  const games = await res.json()
-
-  return NextResponse.json(games)
+  const res = await Owner.upcomingGames(ownerId, addDays)
+  return NextResponse.json(res)
 }
--- app/predictions/create/page.js
+++ app/predictions/create/page.js
@@ -7,9 +7,29 @@ import { useEffect, useState } from "react"
 export default function PredictionsCreate() {
   const {data: session} = useSession({required: true})
   const [day, setDay] = useState(0)
+  const [games, setGames] = useState({})
+
+  function seeGames() {
+    console.log('GAMES', games)
+  }
 
   useEffect(() => {
     console.log('getting games for day:', day)
+
+    async function fetchUpcoming() {
+      const res = await fetch(`/api/games?ownerId=${session.user.owner.id}&addDays=${day}`);
+      const newGames = await res.json()
+      if (!ignore) {
+        setGames(g => ({...g, ...newGames}))
+      }
+
+    }
+
+    session && fetchUpcoming()
+    let ignore = false
+    return () => {
+      ignore = true
+    }
   }, [session, day])
 
   return (
@@ -19,7 +39,17 @@ export default function PredictionsCreate() {
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         Predictions Create. You shouldn&apos;t be here without logging in
       </div>
+      <div>
+        <form>
+          {Object.entries(games).map(([gameId ,game]) => (
+            <p key={gameId}>
+              <span>{game.homeTeam}</span> VS <span>{game.awayTeam}</span>
+            </p>
+          ))}
+        </form>
+      </div>
       <button onClick={() => setDay(day + 1)}>Next Day</button>
+      <button onClick={seeGames}>See Games</button>
     </main>
   )
 }
