3bd3d8f68673f8a72e481bfdb4e1b09a29a227a6
Tyler Martin <tyler.martin@electric-playground.com>
2023-06-28 22:52:00 -0300

    adds button classes

--- app/predictions/create/page.js
+++ app/predictions/create/page.js
@@ -43,13 +43,13 @@ export default function PredictionsCreate() {
         <form>
           {Object.entries(games).map(([gameId ,game]) => (
             <p key={gameId}>
-              <span>{game.homeTeam}</span> VS <span>{game.awayTeam}</span>
+              <span>{game.homeTeam}</span> AT <span>{game.awayTeam}</span>
             </p>
           ))}
         </form>
       </div>
-      <button onClick={() => setDay(day + 1)}>Next Day</button>
-      <button onClick={seeGames}>See Games</button>
+      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={() => setDay(day + 1)}>Load More</button>
+      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={seeGames}>See Games</button>
     </main>
   )
 }
