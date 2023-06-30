7d2c2302fb33f8aa503624a0170d8b69419b757c
Tyler Martin <tyler.martin@electric-playground.com>
2023-06-30 13:07:13 -0300

    completes basic prediction saving logic

--- app/api/predictions/route.js
+++ app/api/predictions/route.js
@@ -11,5 +11,13 @@ export async function GET(request) {
 }
 
 export async function POST(request) {
-  console.log("you've submitted a request to save predictions")
+  const { searchParams } = new URL(request.url)
+  let formData = await request.formData()
+  const ownerId = parseInt(searchParams.get('ownerId'))
+  const savedCount = await Owner.createPredictions(ownerId, formData)
+  console.log('predictions SAVED:', savedCount)
+
+  let dashboardUrl = new URL('/', request.url)
+  return NextResponse.redirect(dashboardUrl, 303)
+  
 }
--- app/predictions/create/page.js
+++ app/predictions/create/page.js
@@ -1,11 +1,13 @@
 'use client'
+import LoadingTemplate from "@/app/components/LoadingTemplate"
 import { useSession } from "next-auth/react"
+import { redirect } from "next/navigation"
 import { useEffect, useState } from "react"
 
  // use client so that we load the page, fetch some upcoming games, then we have a button that allows more games to be loaded
 
 export default function PredictionsCreate() {
-  const {data: session} = useSession({required: true})
+  const {data: session, status} = useSession({required: true})
   const [day, setDay] = useState(0)
   const [games, setGames] = useState({})
 
@@ -32,20 +34,39 @@ export default function PredictionsCreate() {
     }
   }, [session, day])
 
+  if (status === 'loading') {
+    return (
+      <LoadingTemplate/>
+    )
+  }
+  if (status === 'unauthenticated') {
+    return (
+      <main>not logged in</main>
+    )
+  }
   return (
     <main>
-      
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        Predictions Create. You shouldn&apos;t be here without logging in
-      </div>
       <div>
-        <form>
+        <form method="post" action={`/api/predictions?ownerId=${session.user.owner.id}`}>
           {Object.entries(games).map(([gameId ,game]) => (
             <p key={gameId}>
-              <span>{game.homeTeam}</span> AT <span>{game.awayTeam}</span>
+              <label htmlFor={gameId}><span>{game.homeTeam}</span> @ <span>{game.awayTeam}</span></label>
+              {
+                game.prediction ? <span>You have already bet on this</span> :
+                  game.options ? 
+                  <select name={gameId}>
+                    <option value=''>no predictions</option>
+                    {Object.entries(game.options).map(([id, text]) =>(
+                      <option key={id} value={id}>{text}</option>
+                    ))}
+                  </select>
+                  : 
+                  <span>Game has already started</span>
+              }
+
             </p>
           ))}
+          <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" type="submit">Save Predictions</button>
         </form>
       </div>
       <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={() => setDay(day + 1)}>Load More</button>
