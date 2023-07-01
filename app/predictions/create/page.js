71fcdffc50108f71218860ec4f3ff38606edef70
Tyler Martin <tyler.martin@electric-playground.com>
2023-07-01 15:06:35 -0300

    improves style and layout for smaller screen sizes on predictions create page

--- app/predictions/create/page.js
+++ app/predictions/create/page.js
@@ -58,18 +58,23 @@ export default function PredictionsCreate() {
   }
   return (
     <main>
-      <div>
+      <div className="flex justify-around py-2">
         <form id="createPredictions" method="post" action={`/api/predictions?ownerId=${session.user.owner.id}`}>
           {games.map((game) => {
             if (game.id === undefined) {
-              return (<p key={game.message.replaceAll(' ', '_')}>{game.message}</p>)
+              return (<p className="text-center text-neutral-600 font-semibold border-b border-indigo-600" key={game.message.replaceAll(' ', '_')}>{game.message}</p>)
             } else {
-              return (<p key={game.id}>
-              <label htmlFor={game.id}><span>{game.homeTeam}</span> @ <span>{game.awayTeam}</span></label>
+              return (<p className="flex flex-col py-2" key={game.id}>
+              <label htmlFor={game.id}>
+                <div className="px-3 border-2 rounded border-indigo-600 ">
+                  <span>{game.homeTeam}</span> @ <span>{game.awayTeam}</span>
+                </div>
+                <div className="text-sm text-neutral-600">{new Date(game.date).toDateString()}</div>
+              </label>
               {
-                game.prediction ? <span>You have already bet on this</span> :
+                game.prediction ? <span className="text-neutral-600 font-light">You have already bet on this</span> :
                   game.options ? 
-                  <select name={game.id}>
+                  <select className="rounded py-2 px-3 bg-white" name={game.id}>
                     <option value=''>no predictions</option>
                     {Object.entries(game.options).map(([id, text]) =>(
                       <option key={id} value={id}>{text}</option>
@@ -84,9 +89,11 @@ export default function PredictionsCreate() {
           })}
         </form>
       </div>
-      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={() => setDay(day + 1)}>Load More</button>
-      {Object.entries(games).length > 0 &&<button form="createPredictions" className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" type="submit">Save Predictions</button>}
-      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={seeGames}>See Games</button>
+      <div  className="flex justify-around py-2">
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={() => setDay(day + 1)}>Load More</button>
+        {Object.entries(games).length > 0 &&<button form="createPredictions" className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" type="submit">Save Predictions</button>}
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={seeGames}>See Games</button>
+      </div>
     </main>
   )
 }
