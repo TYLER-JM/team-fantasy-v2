799e97380e5c81390f7e2901607a64af7eed27f7
Tyler Martin <tyler.martin@electric-playground.com>
2023-06-30 22:28:38 -0300

    changes 'games' state to array to allow for 'no games' message to be displayed in proper order

--- app/predictions/create/page.js
+++ app/predictions/create/page.js
@@ -1,7 +1,6 @@
 'use client'
 import LoadingTemplate from "@/app/components/LoadingTemplate"
 import { useSession } from "next-auth/react"
-import { redirect } from "next/navigation"
 import { useEffect, useState } from "react"
 
  // use client so that we load the page, fetch some upcoming games, then we have a button that allows more games to be loaded
@@ -9,7 +8,7 @@ import { useEffect, useState } from "react"
 export default function PredictionsCreate() {
   const {data: session, status} = useSession({required: true})
   const [day, setDay] = useState(0)
-  const [games, setGames] = useState({})
+  const [games, setGames] = useState([])
 
   // for development help
   function seeGames() {
@@ -22,8 +21,20 @@ export default function PredictionsCreate() {
     async function fetchUpcoming() {
       const res = await fetch(`/api/predictions?ownerId=${session.user.owner.id}&addDays=${day}`);
       const newGames = await res.json()
+
+      const arrayOfNewGames = Object.entries(newGames).map(([,value]) => value)
+
+      // const emptyDate = new Date()
+      const emptyDate = new Date('2023-04-11') // for development
+      emptyDate.setDate(emptyDate.getDate() + Number(day))
+      let emptyDay = {message: `no games on: ${emptyDate.toDateString()}`}
+
+      if (arrayOfNewGames.length === 0) {
+        arrayOfNewGames.push(emptyDay)
+      }
+
       if (!ignore) {
-        setGames(g => ({...g, ...newGames}))
+        setGames(g => ([...g, ...arrayOfNewGames]))
       }
 
     }
@@ -48,14 +59,17 @@ export default function PredictionsCreate() {
   return (
     <main>
       <div>
-        <form method="post" action={`/api/predictions?ownerId=${session.user.owner.id}`}>
-          {Object.entries(games).map(([gameId ,game]) => (
-            <p key={gameId}>
-              <label htmlFor={gameId}><span>{game.homeTeam}</span> @ <span>{game.awayTeam}</span></label>
+        <form id="createPredictions" method="post" action={`/api/predictions?ownerId=${session.user.owner.id}`}>
+          {games.map((game) => {
+            if (game.id === undefined) {
+              return (<p key={game.message.replaceAll(' ', '_')}>{game.message}</p>)
+            } else {
+              return (<p key={game.id}>
+              <label htmlFor={game.id}><span>{game.homeTeam}</span> @ <span>{game.awayTeam}</span></label>
               {
                 game.prediction ? <span>You have already bet on this</span> :
                   game.options ? 
-                  <select name={gameId}>
+                  <select name={game.id}>
                     <option value=''>no predictions</option>
                     {Object.entries(game.options).map(([id, text]) =>(
                       <option key={id} value={id}>{text}</option>
@@ -65,12 +79,13 @@ export default function PredictionsCreate() {
                   <span>Game has already started</span>
               }
 
-            </p>
-          ))}
-          <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" type="submit">Save Predictions</button>
+            </p>)
+            }
+          })}
         </form>
       </div>
       <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={() => setDay(day + 1)}>Load More</button>
+      {Object.entries(games).length > 0 &&<button form="createPredictions" className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" type="submit">Save Predictions</button>}
       <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={seeGames}>See Games</button>
     </main>
   )
