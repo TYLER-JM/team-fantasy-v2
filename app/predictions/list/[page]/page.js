fa3cd928bb63bd88d4e5ea4be98e330424895a4a
Tyler Martin <tyler.martin@electric-playground.com>
2023-07-02 21:57:51 -0300

    updates classes on predictions list table

--- app/predictions/list/[page]/page.js
+++ app/predictions/list/[page]/page.js
@@ -30,7 +30,7 @@ const PredictionListPage = async ({ params }) => {
             <tr className="border-b dark:border-neutral-500" key={row.date + row.finalScore}>
               <td className="whitespace-nowrap px-3 py-2">{row.date}</td>
               <td className="whitespace-nowrap px-3 py-2">{row.prediction}</td>
-              <td className="whitespace-nowrap px-3 py-2">{row.finalScore}</td>
+              <td className="whitespace-pre-line px-3 py-2">{row.finalScore}</td>
               <td className="whitespace-nowrap px-3 py-2">{row.result}</td>
             </tr>
           ))}
