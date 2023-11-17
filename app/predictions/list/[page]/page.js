90f1e3867aa56698de9dd363fdd75f8ba69056b9
Tyler Martin <tyler.martin@electric-playground.com>
2023-11-17 13:48:55 -0400

    adds toString() method to predictions list page

--- app/predictions/list/[page]/page.js
+++ app/predictions/list/[page]/page.js
@@ -28,7 +28,7 @@ const PredictionListPage = async ({ params }) => {
 
           {predictions.map(row => (
             <tr className="border-b dark:border-neutral-500" key={row.date + row.finalScore}>
-              <td className="whitespace-nowrap px-3 py-2">{row.date}</td>
+              <td className="whitespace-nowrap px-3 py-2">{row.date.toString()}</td>
               <td className="whitespace-nowrap px-3 py-2">{row.prediction}</td>
               <td className="whitespace-pre-line px-3 py-2">{row.finalScore}</td>
               <td className="whitespace-nowrap px-3 py-2">{row.result}</td>
