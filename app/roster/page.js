51556dcec5a7b698ab39ffc7a85dc26d3cdde78b
Tyler Martin <tyler.martin@electric-playground.com>
2024-02-03 20:57:36 -0400

    updates logic on roster page for selected roster

--- app/roster/page.js
+++ app/roster/page.js
@@ -7,17 +7,28 @@ import Roster from "../components/Roster"
 const RosterPage = async () => {
 
   const {data: session, status} = useSession({required: true})
-  const [activeOwner, setActiveOwner] = useState(0)
+  const [selectedOwner, setSelectedOwner] = useState(0)
   const [owners, setOwners] = useState([])
+  const [ownerId, setOwnerId] = useState(false)
+
+  console.log('Session', session)
 
   useEffect(() => {
-    if (session && session.user.owner) {
-      setActiveOwner(session.user.owner.id)
+    if (session && session.user.owner.id !== ownerId) {
+      setOwnerId(session.user.owner.id)
     }
-  }, [session])
+  }, [session, ownerId])
+
+  useEffect(() => {
+    if (ownerId) {
+      console.log('reupdating loggedin user')
+      setSelectedOwner(ownerId)
+    }
+  }, [ownerId])
 
   useEffect(() => {
     async function fetchRosters() {
+      console.log('fetching from DB')
       const res = await fetch(`api/rosters`)
       const ownersWithRosters = await res.json()
       if (!ignore) {
@@ -42,10 +53,10 @@ const RosterPage = async () => {
     return (
       <div className="sm:p-16 overflow-x-auto container mx-auto px-4">
         {owners.map(owner => {
-          return (<button key={owner.id} className={`m-2 px-2 p-1 rounded border border-indigo-700 hover:text-stone-200 hover:bg-indigo-700 ${activeOwner === owner.id ? 'text-stone-200 bg-indigo-600' : ''}`} onClick={() => setActiveOwner(owner.id)}>{owner.name}</button>)
+          return (<button key={owner.id} className={`m-2 px-2 p-1 rounded border border-indigo-700 hover:text-stone-200 hover:bg-indigo-700 ${selectedOwner === owner.id ? 'text-stone-200 bg-indigo-600' : ''}`} onClick={() => setSelectedOwner(owner.id)}>{owner.name}</button>)
         })}
         <div>
-          {owners.filter(owner => owner.id === activeOwner).map(owner => {
+          {owners.filter(owner => owner.id === selectedOwner).map(owner => {
             return <Roster key={owner.id} roster={owner.ownerTeamHistory}/>
           })}
         </div>
